db9fb41ce4c3658d6457e157aa179150194eda97
Nicolas Julian <33948000+nicolasjulian@users.noreply.github.com>
2019-02-01 11:27:02 +0700

    Update Alaram.sh

--- Alaram.sh
+++ Alaram.sh
@@ -8,7 +8,7 @@ hari=`date '+%Y-%m-%d'`
 bulan=`date '+%Y/%m'`
 if [ ! -f /home/$user/Renungan/$hari'.mp3' ]; then
     echo "Download Renungan Hari ini"
-    get_link_renungan=`curl -s dailyfreshjuice.net | grep -Po '(href=")[^"]*' | grep 'http://media.blubrry.com/dailyfreshjuice/p/dailyfreshjuice.net/wp-content/uploads/'$bulan'/FJ'$hari'' | sort -u | sed 's/\href=//g' | tr -d '"'`
+    get_link_renungan=`curl -s dailyfreshjuice.net | grep -Po '(href=")[^"]*' | grep 'http://media.blubrry.com/dailyfreshjuice/p/dailyfreshjuice.net/wp-content/uploads/2019/01/FJ'$hari'' | sort -u | sed 's/\href=//g' | tr -d '"'`
 	simpan_file=`wget $get_link_renungan -O /home/$user/Renungan/$hari'.mp3' -q --show-progress`
 	notify-send "Renungan Harian" "`date`"
 	putar=`play /home/$user/Renungan/$hari'.mp3'`
