159a791aca05586ea0d11bab528676201e391ccb
Nicolas Julian <33948000+nicolasjulian@users.noreply.github.com>
2019-08-22 10:30:13 -0500

    Fix Daily Reading
    Curl to another rss index daily fresh juice

--- Alaram.sh
+++ Alaram.sh
@@ -5,6 +5,7 @@
 
 #Make sure "Play" Env already installed
 echo "Update CLI Player & Install if Nothing"
+echo "+===========================================+"
 sudo apt install sox libsox-fmt-mp3 -y
 
 user=`whoami`
@@ -15,21 +16,25 @@ tahun=`date '+%Y'`
 		#Checking is file ready
 	if [[ ! -d /home/$user/Renungan/ ]]; then
    		echo "Creating $folderFile/ folder"
-   		mkdir /home/$user/Renungan/
+			mkdir /home/$user/Renungan/
+			echo "Silahkan Restart Tools Ini"
   	else
+		echo "+===========================================+"
 		echo  "File Renungan Tersimpan Di /home/$user/Renungan/"
     fi
     		#Curl To Daily Fresh Juice (dot) net
-    		date=`date '+%d %B %Y'`
+    		date=`date '+%d'`
 			tema_date="$date"
-			get_tema=`curl -s 'http://dailyfreshjuice.net'`
-			tema_today=`echo $get_tema | grep -Po '(title=")[^"]*' | grep "$tema_date" | sort -u | grep -Po '(&#8211; )[^"]*' | sed -r 's|&#8211; ||g'`
-			echo "Tema dan Bacaan Hari ini : " $tema_today
+			get_link_download=`curl -s 'http://dailyfreshjuice.net'`
+			tema_today=$(curl -s "http://dailyfreshjuice.net/feed/podcast/" | grep '<title>' | grep 'Juice '$tema_date'' | awk -F ';' '{print $2}' | sed -r "s/^ //g" | sed -r "s|</title>||g")
+			echo "+===========================================+"
+			echo "Tema dan Bacaan Hari ini :  $tema_today"
+			echo "+===========================================+"
 
 			    		#Downloading file rennungan
 					if [[ ! -f /home/$user/Renungan/$hari'.mp3' ]]; then
 						    echo "Download Renungan Hari ini"
-						    get_link_renungan=`echo $get_tema | grep -Po '(href=")[^"]*' | grep 'http://media.blubrry.com/dailyfreshjuice/p/dailyfreshjuice.net/wp-content/uploads/'$tahun'/' | grep 'FJ'$hari'' | sort -u | sed 's/\href=//g' | tr -d '"'`
+						    get_link_renungan=`echo $get_link_download | grep -Po '(href=")[^"]*' | grep 'http://media.blubrry.com/dailyfreshjuice/p/dailyfreshjuice.net/wp-content/uploads/'$tahun'/' | grep 'FJ'$hari'' | sort -u | sed 's/\href=//g' | tr -d '"'`
 							echo $get_link_renungan
 							simpan_file=`wget $get_link_renungan -O /home/$user/Renungan/$hari'.mp3' -q --show-progress`
 							notify-send "Renungan Harian" "`date`"
